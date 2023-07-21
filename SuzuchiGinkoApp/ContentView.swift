2d22696b773fec0ac7ea72d62686061120cb0a79
Agata Bogacz <agatabogacz@outlook.com>
2023-07-22 07:26:43 +0900

    Initial Commit

---
+++ SuzuchiGinkoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuzuchiGinkoApp
+//
+//  Created by Agata Bogacz on 22/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuzuchiGinkoApp/SuzuchiGinkoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuzuchiGinkoAppApp.swift
+//  SuzuchiGinkoApp
+//
+//  Created by Agata Bogacz on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuzuchiGinkoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
